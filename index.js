45e978c181af456a57463bde914e35d48265ffcb
tarik-tk2 <36367834+tarik-tk2@users.noreply.github.com>
2023-05-24 02:00:56 +0600

    2 files modified

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 // Import stylesheets
 import './style.css';
 
- const phoneDisplayId= document.getElementById("display-phone");
 
-const loadPhones= async()=>{
-     const URL='https://openapi.programming-hero.com/api/phones?search=iphone';
-     const response=await fetch(URL);
-     const result =await response.json();
-     displayPhone(result.data)
-     
-}
-const displayPhone=phones=>{
 
-for( const phone of phones){
-  const createDiv= document.createElement("div");
-  createDiv.classList.add("col")
-  createDiv.innerHTML=`
+const loadPhones = async (name) => {
+  const URL = `https://openapi.programming-hero.com/api/phones?search=${name}`;
+  const response = await fetch(URL);
+  const result = await response.json();
+  displayPhone(result.data)
+  
+};
+const displayPhone = (phones) => {
+  const phoneDisplayId = document.getElementById('display-phone');
+  phoneDisplayId.textContent='';
+  for (const phone of phones) {
+    const createDiv = document.createElement('div');
+    createDiv.classList.add('col');
+    createDiv.innerHTML = `
   <div class="card h-100 p-2">
       <img src=${phone.image} class="card-img-top" alt="...">
       <div class="card-body">
@@ -23,11 +24,19 @@ for( const phone of phones){
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       </div>
     </div>
-  `
-  phoneDisplayId.appendChild(createDiv)
-}
-
+  `;
+    phoneDisplayId.appendChild(createDiv);
+  }
   
-   
-}
-loadPhones()
+};
+
+document.getElementById("search-button").addEventListener("click",()=>{
+   const getPhonename= document.getElementById("search-input").value;
+  loadPhones(getPhonename)
+})
+
+const notFound=value=>{
+     const notFound= document.getElementById("not-found");
+     notFound.classList.remove('d-none')
+  }
+
